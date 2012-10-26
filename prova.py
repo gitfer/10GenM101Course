74c657b2ef103c761562ac7df699db31ab8437ec
Federico Rota <federico.rota01@gmail.com>
2012-10-26 22:04:03 +0200

    Eccezioni

--- prova.py
+++
@@ -1,7 +0,0 @@
-from bottle import route, run, template
-
-@route('/hello/:name')
-def index(name='World'):
-    return template('<b>Hello {{name}}</b>!', name=name)
-
-run(host='localhost', port=8080)
