c3167da41028d91e0b35bf21a9bb49e19cc13c4d
Federico Rota <federico.rota01@gmail.com>
2012-10-26 22:35:50 +0200

    Templates

---
+++ 1st_week_views.py
@@ -0,0 +1,9 @@
+import bottle
+
+@bottle.route("/")
+def home_page():
+	mythings = ['apple', 'orange', "pear"]
+	return bottle.template('hello_world', username="Federico", stuff=mythings)
+
+bottle.debug(True)
+bottle.run(host="localhost", port=8080)
