27a7f071496edb83c46b1ba283d256e7668cf82e
Federico Rota <federico.rota01@gmail.com>
2012-10-26 23:12:31 +0200

    Forms via cookie

--- 1st_week_form.py
+++ 1st_week_form.py
@@ -11,6 +11,13 @@ def favourite_fruit():
 	fruit = bottle.request.forms.get("fruit")
 	if(fruit == None or fruit == ""):
 		fruit = "No fruit selected"
+	bottle.response.set_cookie("fruit", fruit)
+	bottle.redirect("/show_fruit")
+#	return bottle.template("result_tpl", {'fruit': fruit})
+
+@bottle.get("/show_fruit")
+def show_fruit():
+	fruit = bottle.request.get_cookie("fruit")
 	return bottle.template("result_tpl", {'fruit': fruit})
 
 bottle.debug(True)
